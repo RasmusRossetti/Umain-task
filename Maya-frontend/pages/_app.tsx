097769f87aea3800b67d243b61ba255920406d7f
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2023-04-12 10:04:16 +0200

    update

--- Maya-frontend/pages/_app.tsx
+++ Maya-frontend/pages/_app.tsx
@@ -1,13 +1,13 @@
-import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/Navbar";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import React, { useState } from "react";
+import Footer from "@/components/footer/Footer"
+import Navbar from "@/components/navbar/Navbar"
+import type { AppProps } from "next/app"
+import React, { useState } from "react"
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("")
   return (
-    <div className='font-Bebas tracking-widest'>
+    <div className="font-Bebas tracking-widest">
       <Navbar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
       <Component
         {...pageProps}
@@ -16,5 +16,5 @@ export default function App({ Component, pageProps }: AppProps) {
       />
       <Footer />
     </div>
-  );
+  )
 }
