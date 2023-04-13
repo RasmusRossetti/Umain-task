3168919b6dda9c811be4f2f834ecdc09f74e0a41
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2023-04-13 20:14:06 +0200

    Head

--- Maya-frontend/pages/_app.tsx
+++ Maya-frontend/pages/_app.tsx
@@ -2,12 +2,16 @@ import Footer from "@/components/footer/Footer"
 import Navbar from "@/components/navbar/Navbar"
 import type { AppProps } from "next/app"
 import React, { useState } from "react"
+import Head from "next/head"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   const [searchQuery, setSearchQuery] = useState("")
   return (
     <div className="font-Bebas tracking-widest">
+      <Head>
+        <title>MAYA</title>
+      </Head>
       <Navbar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
       <Component
         {...pageProps}
